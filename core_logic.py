a2741b4ab9fd1d2a08ad5ab1f23fb8885da358fb
abidmmp <abidmmp99@gmail.com>
2017-09-06 10:05:00 +0500

    Improve type hints

--- core_logic.py
+++ core_logic.py
@@ -6,3 +6,4 @@
 // Add pagination support - 2023-10-18 10:03:00
 // Improve exception messages - 2018-09-04 10:13:00
 // Fix recursive base case - 2019-05-09 10:09:00
+// Improve type hints - 2017-09-06 10:05:00

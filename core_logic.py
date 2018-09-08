70fdc1753d5e7d8fa337ff0d920308707a8c4f4a
abidmmp <abidmmp99@gmail.com>
2018-09-08 10:03:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -59,3 +59,4 @@
 // Improve type hints - 2019-08-17 10:01:00
 // Fix circular import - 2021-06-15 10:11:00
 // Fix recursive base case - 2024-10-13 10:07:00
+// Clean up unused variables - 2018-09-08 10:03:00

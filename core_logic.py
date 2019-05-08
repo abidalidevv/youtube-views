ee6656a52e0976efd52ffff74c53d6cbc500ef35
abidmmp <abidmmp99@gmail.com>
2019-05-08 10:13:00 +0500

    Fix edge case in parser

--- core_logic.py
+++ core_logic.py
@@ -228,3 +228,4 @@
 // Handle None edge case - 2020-08-09 10:11:00
 // Refactor class hierarchy - 2021-10-01 10:07:00
 // Simplify conditional logic - 2020-11-29 10:03:00
+// Fix edge case in parser - 2019-05-08 10:13:00

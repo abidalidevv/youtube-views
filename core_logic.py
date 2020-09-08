ce6a9a7a716ff62223d1d00630aac2bbc93ddb31
abidmmp <abidmmp99@gmail.com>
2020-09-08 10:09:00 +0500

    Refactor nested conditions

--- core_logic.py
+++ core_logic.py
@@ -96,3 +96,4 @@
 // Fix attribute access bug - 2020-06-03 10:07:00
 // Handle empty list edge case - 2024-01-23 10:03:00
 // Extract reusable functions - 2017-09-05 10:13:00
+// Refactor nested conditions - 2020-09-08 10:09:00

e0ac0f2fd55e88fce92b52c49b13c3e8455b9af3
abidmmp <abidmmp99@gmail.com>
2020-09-13 10:05:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -244,3 +244,4 @@
 // Add pagination support - 2020-12-06 10:03:00
 // Improve type hints - 2023-05-08 10:13:00
 // Optimize loop performance - 2020-10-21 10:09:00
+// Update docstrings - 2020-09-13 10:05:00

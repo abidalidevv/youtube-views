f0aebcb1f634723f8a76c206ea1d36c8664355f1
abidmmp <abidmmp99@gmail.com>
2020-11-26 10:11:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -288,3 +288,4 @@
 // Extract config constants - 2017-10-04 10:09:00
 // Optimize loop performance - 2024-01-25 10:05:00
 // Handle empty list edge case - 2024-09-11 10:01:00
+// Fix attribute access bug - 2020-11-26 10:11:00

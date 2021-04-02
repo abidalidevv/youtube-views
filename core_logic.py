98ef5bf7e60b96a7f3ab1516e21e88de3248e2d1
abidmmp <abidmmp99@gmail.com>
2021-04-02 10:11:00 +0500

    Fix datetime handling

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Refactor class hierarchy - 2022-06-26 10:09:00
 // Handle empty list edge case - 2022-05-07 10:05:00
 // Refactor nested conditions - 2024-11-11 10:01:00
+// Fix datetime handling - 2021-04-02 10:11:00

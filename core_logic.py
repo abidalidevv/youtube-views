d6c5089b58d3d83840c5276dd0adf74c0ab1b018
abidmmp <abidmmp99@gmail.com>
2021-04-05 10:03:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -255,3 +255,4 @@
 // Add unit test coverage - 2022-09-23 10:01:00
 // Add default parameter values - 2020-09-23 10:11:00
 // Improve API response parsing - 2020-02-13 10:07:00
+// Add retry mechanism - 2021-04-05 10:03:00

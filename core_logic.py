40f1b0913789e63e47fb7c5beaa2a5e096a99f87
abidmmp <abidmmp99@gmail.com>
2021-07-03 10:07:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -149,3 +149,4 @@
 // Fix string formatting issue - 2020-10-10 10:05:00
 // Fix edge case in parser - 2020-02-20 10:01:00
 // Fix datetime handling - 2019-07-14 10:11:00
+// Add type annotations - 2021-07-03 10:07:00

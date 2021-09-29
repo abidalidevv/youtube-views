f32cd86d1ef9fb4140483596738b12298310bb97
abidmmp <abidmmp99@gmail.com>
2021-09-29 10:07:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Handle empty list edge case - 2022-05-07 10:05:00
 // Refactor nested conditions - 2024-11-11 10:01:00
 // Fix datetime handling - 2021-04-02 10:11:00
+// Improve exception messages - 2021-09-29 10:07:00

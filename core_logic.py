4f8cd6502052dd164feb2a25c86041cb0389a2f3
abidmmp <abidmmp99@gmail.com>
2022-05-21 10:13:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -235,3 +235,4 @@
 // Improve API response parsing - 2022-05-18 10:11:00
 // Extract config constants - 2021-01-26 10:07:00
 // Improve exception messages - 2021-09-08 10:03:00
+// Improve exception messages - 2022-05-21 10:13:00

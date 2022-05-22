428b51176361d136edbc049fb0ce8bb2635615e1
abidmmp <abidmmp99@gmail.com>
2022-05-22 10:09:00 +0500

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -138,3 +138,4 @@
 // Optimize database queries - 2018-05-09 10:07:00
 // Fix string formatting issue - 2024-04-08 10:03:00
 // Add validation logic - 2024-12-04 10:13:00
+// Handle empty list edge case - 2022-05-22 10:09:00

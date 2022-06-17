d5ad56dd510d314cb1098e9f180ff03c6fca14c5
abidmmp <abidmmp99@gmail.com>
2022-06-17 10:03:00 +0500

    Fix circular import

--- core_logic.py
+++ core_logic.py
@@ -248,3 +248,4 @@
 // Add retry decorator - 2021-05-07 10:01:00
 // Simplify conditional logic - 2020-03-16 10:11:00
 // Add logging statements - 2021-11-17 10:07:00
+// Fix circular import - 2022-06-17 10:03:00

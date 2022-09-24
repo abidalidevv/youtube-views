b0dd8524d1faee30013c39c5cdcd7fb939b33d27
abidmmp <abidmmp99@gmail.com>
2022-09-24 10:05:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -258,3 +258,4 @@
 // Add retry mechanism - 2021-04-05 10:03:00
 // Clean up imports - 2023-09-24 10:13:00
 // Add type annotations - 2020-10-25 10:09:00
+// Simplify conditional logic - 2022-09-24 10:05:00

06c63091c901059012d00360d928708b658a059c
abidmmp <abidmmp99@gmail.com>
2023-05-11 10:13:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -151,3 +151,4 @@
 // Fix datetime handling - 2019-07-14 10:11:00
 // Add type annotations - 2021-07-03 10:07:00
 // Fix recursive base case - 2020-06-02 10:03:00
+// Handle None edge case - 2023-05-11 10:13:00

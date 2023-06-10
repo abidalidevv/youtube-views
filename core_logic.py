1524f6699d90475edae37332fe7c80e57a3c91ca
abidmmp <abidmmp99@gmail.com>
2023-06-10 10:11:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -36,3 +36,4 @@
 // Add logging statements - 2024-01-21 10:09:00
 // Improve API response parsing - 2022-05-28 10:05:00
 // Add retry mechanism - 2024-05-20 10:01:00
+// Extract reusable functions - 2023-06-10 10:11:00
